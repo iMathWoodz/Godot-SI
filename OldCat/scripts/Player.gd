2b861c01d1d3eeea0d4cd48050594cb63b6e4166
Erica Lopes <33881813+ericaolopess@users.noreply.github.com>
2020-05-19 22:09:47 -0300

    aula 19/05

--- OldCat/scripts/Player.gd
+++ OldCat/scripts/Player.gd
@@ -10,6 +10,8 @@ var y_velo = 0
 
 func _physics_process(delta):
 	var move_dir = 0
+	var grounded = is_on_floor()
+	var no_chao = $RayD.is_colliding() or $RayE.is_colliding()
 	
 	if Input.is_action_pressed("direita"):
 		$AnimatedSprite.animation = "walk"
@@ -25,10 +27,16 @@ func _physics_process(delta):
 		$AnimatedSprite.animation = "idle"
 		
 	
+	if  grounded and Input.is_action_pressed("jump"):
+		y_velo = - JUMP_FORCE
+		$AnimatedSprite.animation = "jump"
+	
 	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0,-1))
 	
 	y_velo += GRAVITY
+	
+	if grounded and y_velo >= 0:
+		y_velo = 5
 		
-		
-		
-		
+	if y_velo > MAX_FALL_SPEED:
+		y_velo = MAX_FALL_SPEED
